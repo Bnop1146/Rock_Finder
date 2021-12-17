b89634e350afbf5a3b16dafd0c25dd9ba2902428
Joachim <joachim.berner@hotmail.com>
2021-12-17 09:05:23 +0100

    Gået Live

--- settings/init.php
+++ settings/init.php
@@ -3,11 +3,11 @@ require "classes/classDB.php";
 
 define("CONFIG_LIVE", "0"); // 0: Test enviroment || 1: Live enviroment
 
-if(CONFIG_LIVE == 0){
-    $DB_SERVER = "localhost";
-    $DB_NAME = "rock_finder";
-    $DB_USER = "root";
-    $DB_PASS = "";
+if(CONFIG_LIVE == 1){
+    $DB_SERVER = "mysql109.unoeuro.com";
+    $DB_NAME = "bnopone_dk_db";
+    $DB_USER = "bnopone_dk";
+    $DB_PASS = "k26RgmedhF4H";
 }else{
     $DB_SERVER = "";
     $DB_NAME = "";
